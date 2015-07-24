549748821a6a6b6b951edcf4207fb5a8fc9ec9ea
DevWurm <devwurm@gmx.net>
2015-07-24 10:18:00 +0200

    Adding Json conversion and Tests

--- src/main/java/de/saxsys/model/Task.java
+++ src/main/java/de/saxsys/model/Task.java
@@ -1,43 +1,64 @@
-package de.saxsys.model;
-
 /**
  * Class for storing information about a task.
  * Includes task title, priority, description (optional) and name of th person in charge for this task (optional).
  */
 
-public class Task {
+package de.saxsys.model;
+
+import java.io.Serializable;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private String title;
     private int priority;
     private String description;
     private String inCharge;
-    
-    /** 
+
+    /**
      * Create a Task without description and person in charge
-     *  @param title Title of the new Task object
-     *  @param priority Priority of the new Task object
+     * 
+     * @param title
+     *            Title of the new Task object
+     * @param priority
+     *            Priority of the new Task object
      */
     public Task(String title, int priority) {
-        this(title, priority, null);       
+        this(title, priority, null);
     }
-    
-    /** 
+
+    /**
      * Create a Task without person in charge
-     *  @param title Title of the new Task object
-     *  @param priority Priority of the new Task object
-     *  @param description description of the new Task object
+     * 
+     * @param title
+     *            Title of the new Task object
+     * @param priority
+     *            Priority of the new Task object
+     * @param description
+     *            description of the new Task object
      */
     public Task(String title, int priority, String description) {
-       this(title, priority, description, null);      
+        this(title, priority, description, null);
     }
-    
-    /** 
+
+    /**
      * Create a Task without person in charge
-     *  @param title Title of the new Task object
-     *  @param priority Priority of the new Task object
-     *  @param description description of the new Task object
-     *  @param inCharge person in charge of the new Task object
-     *  @throws IllegalArgumentException If title is null
-     *  @throws IllegalArgumentException If priority is 0
+     * 
+     * @param title
+     *            Title of the new Task object
+     * @param priority
+     *            Priority of the new Task object
+     * @param description
+     *            description of the new Task object
+     * @param inCharge
+     *            person in charge of the new Task object
+     * @throws IllegalArgumentException
+     *             If title is null
+     * @throws IllegalArgumentException
+     *             If priority is 0
      */
     public Task(String title, int priority, String description, String inCharge) {
         setTitle(title);
@@ -45,82 +66,133 @@ public class Task {
         setDescription(description);
         setInCharge(inCharge);
     }
-    
-    
+
+    public boolean equals(Task comp) {
+        if (this == comp) {
+            return true;
+        } else if (this.getTitle().equals(comp.getTitle()) && this.getPriority() == comp.getPriority()
+                && this.getDescription().equals(comp.getDescription()) && this.getInCharge().equals(comp.getInCharge())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     /**
      * Set a new task title in the Task object
-     * @param title new title for the Task object; null is not allowed
-     * @throws IllegalArgumentException If title is null
+     * 
+     * @param title
+     *            new title for the Task object; null is not allowed
+     * @throws IllegalArgumentException
+     *             If title is null
      */
     public void setTitle(String title) {
         if (title != null) {
             this.title = title;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Title musn't be null");
         }
     }
-    
+
     /**
      * Set a new task priority in the Task object
-     * @param priority new priority for the Task object; 0 is not allowed
-     * @throws IllegalArgumentException If priority is 0
+     * 
+     * @param priority
+     *            new priority for the Task object; 0 is not allowed
+     * @throws IllegalArgumentException
+     *             If priority is 0
      */
     public void setPriority(int priority) {
         if (priority > 0) {
             this.priority = priority;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Priority must be bigger than 0");
         }
     }
-    
+
     /**
      * Set a new task description in the Task object
-     * @param description new priority for the Task object
+     * 
+     * @param description
+     *            new priority for the Task object
      */
     public void setDescription(String description) {
         this.description = description;
     }
-    
+
     /**
      * Set a new person in charge for the task in the Task object
-     * @param inCharge new name of person in charge for the Task object
+     * 
+     * @param inCharge
+     *            new name of person in charge for the Task object
      */
     public void setInCharge(String inCharge) {
         this.inCharge = inCharge;
     }
-    
-    
+
     /**
      * Gets the title of the task
+     * 
      * @return The title as an String
      */
     public String getTitle() {
         return this.title;
     }
-    
+
     /**
      * Gets the priority of the task
+     * 
      * @return The priority as an int
      */
     public int getPriority() {
         return this.priority;
     }
-    
+
     /**
      * Gets the description of the task
+     * 
      * @return The description as an String
      */
     public String getDescription() {
         return this.description;
     }
-    
+
     /**
      * Gets the person in charge of the task
+     * 
      * @return The name of the person in charge as an String
      */
     public String getInCharge() {
         return this.inCharge;
+    }
+
+    /**
+     * Creates an JSON Serialization of the given Task object
+     * 
+     * @param inputTask
+     *            The task objhect to be serialized
+     * @return The JSON String
+     */
+    public static String toJson(Task inputTask) {
+        Gson marshaller = new Gson();
+        return marshaller.toJson(inputTask);
+    }
+
+    /**
+     * Creates a task object from an JSON serialization
+     * 
+     * @param jsonString
+     *            The JSON serialization to be nmarshalled
+     * @return The created Task object
+     * @throws JsonMappingException
+     *             if the parsed data couldn't be mapped to the object
+     * @throws JsonParseException
+     *             if the data couldn't be parsed
+     * @throws IOException
+     *             shouldn't occur
+     */
+    public static Task fromJson(String jsonString) throws JsonSyntaxException {
+        Gson demarshaller = new Gson();
+        return demarshaller.fromJson(jsonString, Task.class);
     }
 }
---
+++ src/test/java/de/saxsys/model/TaskTests.java
@@ -0,0 +1,89 @@
+package de.saxsys.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TaskTests {
+    private Task task;
+    
+    @Before
+    public void before() {
+        task = new Task("Task1", 1, "A standard Task", "Person");
+    }
+    
+    //Equals Test
+    @Test
+    public void testEquals() {
+        Task task1 = task; //same object
+        Task task2 = new Task("Task1", 1, "A standard Task", "Person"); //object that is equal
+        Task task3 = new Task("Task3", 1, "A standard Task", "Person"); //object that is different
+        
+        assertTrue(task.equals(task1));
+        assertTrue(task.equals(task2));
+        assertFalse(task.equals(task3));
+    }
+    
+    //Get Tests
+    @Test
+    public void testGetTitle() {
+        assertEquals("Task1", task.getTitle());
+    }
+    
+    @Test
+    public void testGetPriority() {
+        assertEquals(1, task.getPriority());
+    }
+    
+    @Test
+    public void testGetDescription() {
+        assertEquals("A standard Task", task.getDescription());
+    }
+    
+    @Test
+    public void testGetInCharge() {
+        assertEquals("Person", task.getInCharge());
+    }
+    
+    //Set Tests
+    @Test
+    public void testSetTitle() {
+        task.setTitle("Task2");
+        assertEquals("Task2", task.getTitle());
+    }
+    
+    @Test
+    public void testSetPriority() {
+        task.setPriority(2);
+        assertEquals(2, task.getPriority());
+    }
+    
+    @Test
+    public void testSetDescription() {
+        task.setDescription("Changed Description");
+        assertEquals("Changed Description", task.getDescription());
+    }
+    
+    @Test
+    public void testSetInCharge() {
+        task.setInCharge("Person2");
+        assertEquals("Person2", task.getInCharge());
+    }
+    
+    //Json Tests
+    @Test
+    public void testToJson() {
+        try {
+            assertEquals("{\"title\":\"Task1\",\"priority\":1,\"description\":\"A standard Task\",\"inCharge\":\"Person\"}", Task.toJson(task));
+        }
+        catch (Exception e) {
+            fail();
+        }
+    }
+    
+    @Test
+    public void testFromJson() {
+        Task task2 = Task.fromJson("{\"title\":\"Task1\",\"priority\":1,\"description\":\"A standard Task\",\"inCharge\":\"Person\"}");
+        assertTrue(task.equals(task2));
+    }
+}
